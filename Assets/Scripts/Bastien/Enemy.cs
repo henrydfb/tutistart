32edcad3502dfd9018bfc62d78910f48aafd7327
bmazuc <mazuc.bastien@gmail.com>
2016-09-12 08:55:03 +0900

    Working on enemy behavior and waves. Merge.

---
+++ Assets/Scripts/Bastien/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public int points;
+    public float speed;
+    public float angle;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+    }
+
+    protected bool checkIfOutOfScreen()
+    {
+        Camera camera = Camera.main;
+        if (gameObject.GetComponent<Transform>().position.y <= camera.ScreenToWorldPoint(new Vector3(0, 0, 0)).y)
+            return true;
+        else
+            return false;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Destroy(coll.gameObject);
+        Destroy(gameObject);
+        GameObject.Find("Player").GetComponent<Player>().increaseScore(points);
+        GameObject.Find("EnemyManager").GetComponent<EnemyManager>().increaseEnemyKilled();
+    }
+}
---
+++ Assets/Scripts/Bastien/EnemyUndefined.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUndefined : Enemy {
+
+    float sine = 0f;
+    int direction = 1;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sine += direction * Time.deltaTime;
+
+        if (sine >= 1f || sine <= -1f)
+            direction *= -1;
+
+        gameObject.GetComponent<Transform>().position += new Vector3(-sine * angle * Time.deltaTime, -speed * Time.deltaTime, 0f);
+        if (checkIfOutOfScreen())
+            Destroy(gameObject);
+    }
+}
